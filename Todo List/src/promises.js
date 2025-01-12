c4c0fa309e97f37dea9d9211bc7b7e42066609b6
danielaczarref <daniela.czarref@gmail.com>
2025-01-12 17:31:00 -0300

    todo list promises code and test

---
+++ Todo List/src/promises.js
@@ -0,0 +1,28 @@
+const todolist = require('./todolist')
+
+getAllTodos = () => {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if (!todolist || todolist.length == 0) {
+                reject(new Error('No data'));
+            } else {
+                resolve(todolist);
+            }
+        }, 1000);
+        })
+}
+
+const createTodo = (todo) => {
+    return new Promise((resolve, reject) => {
+        if (!todo || !todo.id || !todo.title) {
+            reject(new Error('No data to be added'));
+        } else {
+            todolist.push(todo);
+            resolve(todo);
+        }
+    });
+};
+
+module.exports ={
+    createTodo,getAllTodos
+}
